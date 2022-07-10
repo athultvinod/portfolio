7952c56de285c999bd12e90bf276131f0377cc4e
athultvinod <102733428+athultvinod@users.noreply.github.com>
2022-07-10 12:56:27 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { MainComponent } from './main/main.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path : "",component : MainComponent},
+{path:"aboutus", component :AboutusComponent },
+{path:"services",component:ServicesComponent},
+{path:"contactus",component:ContactusComponent},
+{path:"skills",component:SkillComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AboutusComponent,
+    ContactusComponent,
+    ServicesComponent,
+    FooterComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
